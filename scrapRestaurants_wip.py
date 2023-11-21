edfb9d7a85bf053c520185b580ea211b12ad70d8
mtufano <74215864+mtufano@users.noreply.github.com>
2023-11-21 19:46:18 +0100

    collecting urls in all london areas

--- scrapRestaurants_wip.py
+++ scrapRestaurants_wip.py
@@ -8,6 +8,8 @@ from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 import random
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
 class ScrapRestaurants:
@@ -15,7 +17,23 @@ class ScrapRestaurants:
     def read_urls_from_file(filename):
         with open(filename, 'r') as file:
             return [line.strip() for line in file]
+    
+    @staticmethod
+    def scroll_to_bottom(driver):
+        last_height = driver.execute_script("return document.body.scrollHeight")
+        while True:
+            # Scroll down to the bottom of the page
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
+            # Wait for the page to load
+            time.sleep(random.uniform(2, 5))
+
+            # Calculate the new scroll height and compare it with the last scroll height
+            new_height = driver.execute_script("return document.body.scrollHeight")
+            if new_height == last_height:
+                break
+            last_height = new_height
+    
     @staticmethod
     def scrape_restaurant_urls(area_urls):
         restaurant_urls = []
@@ -28,7 +46,9 @@ class ScrapRestaurants:
             # Wait for the page to load
             time.sleep(random.uniform(2, 5))
 
-
+            # Scroll down to the bottom of the page
+            ScrapRestaurants.scroll_to_bottom(driver)
+            
             # Find all <a> tags that contain restaurant URLs
             restaurant_links = driver.find_elements(By.CSS_SELECTOR, "a[data-test-id='f-restaurant-card--restaurant-card-component']")
 
@@ -46,9 +66,16 @@ class ScrapRestaurants:
             time.sleep(random.uniform(2, 5))
 
         driver.quit()
+        ScrapRestaurants.save_urls_to_file(restaurant_urls, 'scraped_urls.txt')
         return restaurant_urls
+
+    @staticmethod
+    def save_urls_to_file(urls, file_name):
+        with open(file_name, 'w') as file:
+            for url in urls:
+                file.write(url + '\n')
+        print(f'Saved {len(urls)} URLs to {file_name}')
 
 # Usage
 area_urls = ScrapRestaurants.read_urls_from_file('test.txt')
 scraped_urls = ScrapRestaurants.scrape_restaurant_urls(area_urls)
-print(scraped_urls)
--- urls/URL_collector_london_wip.py
+++ urls/URL_collector_london_wip.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import os
 from fake_useragent import UserAgent
 
